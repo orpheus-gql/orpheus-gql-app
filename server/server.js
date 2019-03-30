3b8fd7deba256a7e1f4fb9cd5b3dc5c5dbd8717c
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-30 10:33:40 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,5 @@ const path = require('path');
 
 app.use('/build', express.static('build'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./index-web.html'));
-});
 
 app.listen(3000, () => console.log('Listening on Port: 3000 .-.'));
