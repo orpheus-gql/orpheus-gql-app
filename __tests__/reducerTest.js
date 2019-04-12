6019d99be32b4cb368b6ce2d3af44bb88654335d
Wisdom Liu <wisdom.code161@gmail.com>
2019-04-11 18:59:24 -0700

    TDD for react and reducers

--- __tests__/reducerTest.js
+++ __tests__/reducerTest.js
@@ -1,5 +1,6 @@
 // const appReducer = require('../client/reducers/appReducer.js')
 import appReducer from '../client/reducers/appReducer'
+import { app } from 'electron';
 describe('App reducer', () => {
   let state;
 
@@ -15,6 +16,8 @@ describe('App reducer', () => {
       dataVis: {
         resolverNum: null,
         resolverNames: [],
+        results: {},
+        visObj: {},
       },
       networkLatency: null,
     };
@@ -31,6 +34,56 @@ describe('App reducer', () => {
       const action = { type: 'aajsbicawlbejckr' };
       expect(appReducer(state, action)).toBe(state);
     });
+  });
+
+  describe('UPDATE_CODE_INPUT', () => {
+    const action = {
+      type: 'UPDATE_CODE_INPUT',
+      payload: '{hello}'
+    };
+    it('updates code input with action payload', () => {
+      const { codeInput } = appReducer(state, action);
+      expect(codeInput).toEqual(action.payload);
+    });
+    it('returns a state object not strictly equal to the original', () => {
+      const newState = appReducer(state, action);
+      expect(newState).not.toBe(state);
+    });
+  });
+
+  describe('BUILD_TREE_VIS', () => {
+    const action = {
+      type: 'BUILD_TREE_VIS',
+      payload: {
+        title: 'author',
+        color: 'blue',
+        value: '50',
+        children: [],
+      }
+    }
+      it('updates dataVis.visObj with action payload', () => {
+        const visObj = appReducer(state, action);
+        expect(visObj.dataVis.visObj).toEqual(action.payload);
+      });
+      it('returns a state object not strictly equal to the original', () => {
+        const newState = appReducer(state, action);
+        expect(newState).not.toBe(state);
+      });
+  });
+
+  describe('STORE_RESPONSE_DATA', () => {
+    const action = {
+      type: 'STORE_RESPONSE_DATA',
+      payload: '50'
+    }
+      it('updates dataVis.results with action payload', () => {
+        const dataVisResults = appReducer(state, action);
+        expect(dataVisResults.dataVis.results).toEqual(action.payload);
+      });
+      it('returns a state object not strictly equal to the original', () => {
+        const newState = appReducer(state, action);
+        expect(newState).not.toBe(state);
+      });
   });
 
 })
